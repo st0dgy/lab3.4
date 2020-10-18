0e1b12a36c03ac2a8f16ff137fb7aa56ca022be1
st0dgy <72568988+st0dgy@users.noreply.github.com>
2020-10-18 21:07:23 +0300

    Add files via upload

---
+++ lab3.4/lab3.4.cpp
@@ -0,0 +1,28 @@
+﻿// Lab_03_4.cpp
+// < Соловей Катерина >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 18
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R;
+	
+	cout << "x = "; cin >> x;
+	cout << "R = "; cin >> R;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if (((y <= sqrt(R * R - pow((x - R), 2) + R) && y <= x) ||
+		((y <= sqrt(R * R - pow((x + R), 2) && y >= -x && y<=0)))))
+		cout << "yes" << endl;
+	else
+
+         cout << "no" << endl;
+		cin.get();
+		return 0;
+}
